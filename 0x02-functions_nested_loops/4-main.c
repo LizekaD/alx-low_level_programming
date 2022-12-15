bdb65806cd9efd06c1dc06098d2f89589819bea9
LizekaD <lizekadada@gmail.com>
2022-12-15 14:00:35 -0800

    t4

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * main - check the code.
@@ -7,16 +7,16 @@ nclude "main.h"
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	r = _isalpha('H');
+	 _putchar(r + '0');
+	 r = _isalpha('o');
+	 _putchar(r + '0');
+	 r = _isalpha(108);
+	 _putchar(r + '0');
+	 r = _isalpha(';');
+	 _putchar(r + '0');
+	 _putchar('\n');
+	 return (0);
 }
