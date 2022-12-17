539984d26d20a1f33517ff5a2d40f8c8ee3ecefb
LizekaD <lizekadada@gmail.com>
2022-12-17 05:58:46 -0800

    t0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests fuction that prints if integer is positive or negetive
+ * main - tests fuction that prints if integer is positive or negative
  * Return: 0
  */
 
