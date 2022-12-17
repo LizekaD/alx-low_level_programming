0d11d54024fcbde281991c25cf65cab2d71c6fb0
LizekaD <lizekadada@gmail.com>
2022-12-17 05:34:22 -0800

    t0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negetive(int i);
 int lagest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, in year);
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 
