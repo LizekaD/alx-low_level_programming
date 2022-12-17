192cb87bf362b0b4137d76a33d718f57b4cdafda
LizekaD <lizekadada@gmail.com>
2022-12-16 16:52:27 -0800

    t11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n <= 98: n++)
+	for (; n <= 98; n++)
 	{
 	if (n == 98)
 	{
@@ -32,7 +32,7 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 	printf("%d", n);
-	print f("\n");
+	printf("\n");
 	break;
 	}
 	else
