9a34bbe6753fcf4671b78198618d32ff58d46bdf
LizekaD <lizekadada@gmail.com>
2022-12-18 01:02:02 -0800

    t101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - prints exactly and piece of art  is useful
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	write(2, "and that piece of art useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
