f0d36ddc81b1b9c747eaa8481a34b9bbc51c07c4
LizekaD <lizekadada@gmail.com>
2022-12-18 04:27:31 -0800

    t3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
-			putchar(c);
-	}
+		putchar(c);
 	for (c = 'A'; c <= 'Z'; c++)
-	{
-		if (c != 'E' && c != 'Q')
-			putchar(c);
-	}
+		putchar(c);
 	putchar('\n');
 	return (0);
 }
