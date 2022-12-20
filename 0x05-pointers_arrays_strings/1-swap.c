d8ebb3eda9e76ae162474d8cce18074bc3f89944
LizekaD <lizekadada@gmail.com>
2022-12-20 13:12:38 -0800

    t1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: First command to be swapped.
+ * @b: second command to be swapped.
+ *
+ * Return: Void
+ */
+int swap_int(int *a, int *b)
+{
+	int *a;
+	int *b;
+
+	*a = *b;
+	*b = p;
+}
