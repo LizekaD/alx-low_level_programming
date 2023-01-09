8179e7a13106d0caaa4aab96d3a24dd568dc7ff2
LizekaD <lizekadada@gmail.com>
2023-01-09 14:39:32 -0800

    t5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _pow_recursion -  returns the value natural square value.
+ * _sqrt_recursion -  returns the value natural square value.
  * @n: number to find square of
  *
  * Return: squareroot of n
  * -1 if n does not have natural square
  */
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else 
-		return (helperfunction(n,0));
+	else
+		return (helperfunction(n, 0));
 }
