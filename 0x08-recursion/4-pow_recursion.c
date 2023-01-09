8f32c9b74293f05e35d5a87e3346acb31f510cea
LizekaD <lizekadada@gmail.com>
2023-01-09 14:24:28 -0800

    t4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  returns the value natural square value.
+ * @n: number to find square of
+ *
+ * Return: squareroot of n
+ * -1 if n does not have natural square
+ */
+int _pow_recursion(int x, int y)
+{
+	if (n < 0)
+		return (-1);
+	else 
+		return (helperfunction(n,0));
+}
