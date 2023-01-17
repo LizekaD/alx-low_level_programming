eb5d9c1d24e628835eb6e0f6f6e7ed468f44aa04
LizekaD <lizekadada@gmail.com>
2023-01-17 14:45:24 -0800

    t5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog -  free dogs
+ * @d: pointer
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
